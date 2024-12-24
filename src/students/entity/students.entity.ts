5fdf2c0a71ae6e1a7a9bdfa1b19a065e617c8481
longnh <longnh.uit@gmail.com>
2024-12-24 22:08:27 +0700

    typeorm: update relation db

--- src/students/entity/students.entity.ts
+++ src/students/entity/students.entity.ts
@@ -1,4 +1,5 @@
-import { Column, Entity, PrimaryGeneratedColumn } from 'typeorm';
+import { ClassEntity } from 'src/classes/entity/class.entity';
+import { Column, Entity, ManyToOne, PrimaryGeneratedColumn } from 'typeorm';
 
 @Entity('students')
 export class StudentEntity {
@@ -9,5 +10,6 @@ export class StudentEntity {
   studentName: string;
 
   @Column()
-  className: string;
+  @ManyToOne(() => ClassEntity, classEntity => classEntity.id)
+  classId: string;
 }
